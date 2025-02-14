05b961c0402b83a6bde90d5c5c3f88bfe1cba855
tayadj <tayadj@users.noreply.github.com>
2025-02-14 22:48:06 +0300

    Data validation pipeline implementation, example scripts added

--- api/application.py
+++ api/application.py
@@ -6,6 +6,23 @@ sys.path.append(sys.path[0]+'\\..')
 
 import src as RecSys
 
-
 help(RecSys)
 
+#
+# Note: Scripts are needed to be moved.
+#
+
+
+
+#
+# Train Example Script
+#
+
+df_s, df_o, df_a = RecSys.data.load('./src/data/raw')
+dvp = RecSys.core.pipeline.DataValidationPipeline()
+df_clean_s = dvp.process(df_s)
+
+
+#
+# Inference Example Script
+#
