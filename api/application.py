fc00ea2ce79fe82b51b7580b436c1c7a695a8f5a
tayadj <tayadj@users.noreply.github.com>
2025-02-15 14:56:00 +0300

    File independence improvement, data validation pipeline exclude-mechanism added

--- api/application.py
+++ api/application.py
@@ -18,9 +18,11 @@ help(RecSys)
 # Train Example Script
 #
 
-df_s, df_o, df_a = RecSys.data.load('./src/data/raw')
-dvp = RecSys.core.pipeline.DataValidationPipeline()
+df_s, df_o, df_a = RecSys.data.load(sys.path[0] + '\\..\\src\\data\\raw')
+dvp = RecSys.core.pipeline.DataValidationPipeline(exclude = ['birth', 'rate', 'timestamp'])
 df_clean_s = dvp.process(df_s)
+df_clean_o = dvp.process(df_o)
+df_clean_a = dvp.process(df_a)
 
 
 #
