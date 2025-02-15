0839cb2bc4090dbfb427a0abee9088ef01b38aed
tayadj <tayadj@users.noreply.github.com>
2025-02-15 16:03:20 +0300

    Data validation and model embedding pipelines development

--- api/application.py
+++ api/application.py
@@ -19,10 +19,14 @@ help(RecSys)
 #
 
 df_s, df_o, df_a = RecSys.data.load(sys.path[0] + '\\..\\src\\data\\raw')
-dvp = RecSys.core.pipeline.DataValidationPipeline(exclude = ['birth', 'rate', 'timestamp'])
+
+dvp = RecSys.core.pipeline.DataValidationPipeline(exclude = ['birth', 'rate', 'timestamp'], time = ['birth', 'timestamp'])
 df_clean_s = dvp.process(df_s)
 df_clean_o = dvp.process(df_o)
 df_clean_a = dvp.process(df_a)
+
+mep = RecSys.core.pipeline.ModelEmbeddingPipeline(df_clean_s,df_clean_o,df_clean_a)
+emb = mep.process()
 
 
 #
