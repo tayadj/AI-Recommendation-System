b4759ebe28eaf96d8ba201cbf4864974ee6945b1
tayadj <tayadj@users.noreply.github.com>
2025-02-19 00:45:30 +0300

    Base data ingestion pipeline structure added

--- src/core/pipeline/data_ingestion_pipeline.py
+++ src/core/pipeline/data_ingestion_pipeline.py
@@ -1,4 +1,32 @@
+import src.data
+import pandas
+import sys
+import os
+
+
 
 class DataIngestionPipeline:
 
-	pass
+	def __init__(self):
+
+		pass
+
+	def ingestion(self, data, config = {}):
+
+		mode = config.get('mode', 'new')
+		version = config.get('version')
+
+		match mode:
+
+			case 'new':
+
+				pass
+
+			case 'append':
+
+				pass
+
+			case _:
+
+				pass
+
