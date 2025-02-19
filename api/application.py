5f2cdba90cbc5be829f8f28d2b439d223021e5d4
tayadj <tayadj@users.noreply.github.com>
2025-02-19 13:15:17 +0300

    API refactoring, inference documentation added

--- api/application.py
+++ api/application.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import List
+from typing import List, Dict, Any
 import pandas
 import sys
 
@@ -19,39 +19,83 @@ application = FastAPI(
 class InferenceRequest(BaseModel):
 
     version: str
-    subject_id: List[int]
-    subject_gender: List[str]
-    subject_birth: List[str]
-    subject_location: List[str]
-    object_id: List[int]
-    object_category: List[str]
-    timestamp: List[str]
+    config: Dict[str, str]
+    data: Dict[str, List[Any]]
 
 @application.post("/inference")
 def inference(request: InferenceRequest):
 
+    """
+    Endpoint: 
+
+        POST /inference
+
+    Request:
+
+        Headers:
+            Content-Type: application/json
+
+        Body Parameters:
+            version (string): Version of the model to use for inference.
+            config (object): Dictionary of configuration parameters.
+            data (object): Dictionary of features, where each key is a feature name and the value is a list of corresponding feature values.
+
+        Request body:
+        {
+	        "version": "sequential",
+	        "config": {},
+	        "data": {
+		        "subject_id": [ 4, 1 ],
+		        "subject_gender": [ "m", "m" ],
+		        "subject_birth": [ "1988-06-30", "2014-09-11" ],
+		        "subject_location": [ "Tokyo", "Berlin" ],
+		        "object_id": [ 3, 1 ],
+		        "object_category": [ "Sport", "Sport" ],
+		        "timestamp": [ "2026-02-17", "2026-02-17" ]
+	        }
+        }
+
+    Response:
+    
+        Status Code: 200 OK
+        Response Body:
+        {
+            "inference": [[-0.49940162897109985],[0.9917365908622742]]
+        }
+
+        Status Code: 500 Internal Server error
+        Response Body:
+        {
+            "detail": "Error message"
+        }
+
+    """
+
     try:
 
         version = request.version
-        data = request.model_dump()
-        data.pop("version")
+        config = request.config
+        data = request.data
         data = pandas.DataFrame(data)
 
         model_inference_pipeline = RecSys.core.pipeline.ModelInferencePipeline(version)
         response = model_inference_pipeline.process(data)
         response = response.tolist()
 
-        return {"inference": response}
+        return { "inference": response }
 
-    except Exception:
+    except Exception as exception:
 
-        raise HTTPException(status_code = 500, detail = str(Exception))
+        raise HTTPException(status_code = 500, detail = str(exception))
+
 
 
 
 class IngestionRequest(BaseModel):
 
-    pass
+    version: str
+    config: Dict[str, str]
+    data: Dict[str, Dict[str, List[Any]]]
 
 @application.post("/ingestion")
 def ingestion(request: IngestionRequest):
@@ -74,9 +118,9 @@ def build(request: BuildRequest):
 
         return {"status": "OK"}
 
-    except Exception:
+    except Exception as exception:
 
-        raise HTTPException(status_code = 500, detail = str(Exception))
+        raise HTTPException(status_code = 500, detail = str(exception))
 
     
 
