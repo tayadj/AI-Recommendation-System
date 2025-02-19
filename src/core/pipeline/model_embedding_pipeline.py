a0d441073c076e95eba64dde9432456236f34352
tayadj <tayadj@users.noreply.github.com>
2025-02-19 19:05:23 +0300

    Unification pipelines

--- src/core/pipeline/model_embedding_pipeline.py
+++ src/core/pipeline/model_embedding_pipeline.py
@@ -9,9 +9,10 @@ class ModelEmbeddingPipeline:
 
 	class Dataset(torch.utils.data.Dataset):
 
-		def __init__(self, data):
+		def __init__(self, data, config):
 
 			self.data = data
+			self.version = config.get('version')
 
 		def __len__(self):
 
@@ -21,70 +22,96 @@ class ModelEmbeddingPipeline:
 
 			record = self.data.iloc[index]
 
-			return {
-				'subject_id': torch.tensor(record['subject_id'], dtype=torch.long),
-				'subject_gender': torch.tensor(record['subject_gender'], dtype=torch.long),
-				'subject_age': torch.tensor(record['subject_age'], dtype=torch.float),
-				'subject_location': torch.tensor(record['subject_location'], dtype=torch.long),
-				'object_id': torch.tensor(record['object_id'], dtype=torch.long),
-				'object_category': torch.tensor(record['object_category'], dtype=torch.long),
-				'rate': torch.tensor(record['rate'], dtype=torch.float),
-			}
+			match self.version:
+
+				case 'base':
+
+					return {
+						'subject_id': torch.tensor(record['subject_id'], dtype=torch.long),
+						'subject_gender': torch.tensor(record['subject_gender'], dtype=torch.long),
+						'subject_age': torch.tensor(record['subject_age'], dtype=torch.float),
+						'subject_location': torch.tensor(record['subject_location'], dtype=torch.long),
+						'object_id': torch.tensor(record['object_id'], dtype=torch.long),
+						'object_category': torch.tensor(record['object_category'], dtype=torch.long),
+						'rate': torch.tensor(record['rate'], dtype=torch.float),
+					}
             
         
 
 
-	def __init__(self, data_subject, data_object, data_action, config = {}):
+	def __init__(self, config = {}):
 
-		self.data_subject = data_subject
-		self.data_object = data_object
-		self.data_action = data_action
+		self.version = config.get('version')
 
 		self.batch_size = config.get('batch_size', 4)
 
 	def featuring(self):
 
-		current_date = pandas.to_datetime('today')
-		self.data_subject['age'] = self.data_subject['birth'].apply(lambda value: current_date.year - value.year - ((current_date.month, current_date.day) < (value.month, value.day)))
+		match self.version:
+
+			case 'base':
+
+				current_date = pandas.to_datetime('today')
+				self.data_subject['age'] = self.data_subject['birth'].apply(lambda value: current_date.year - value.year - ((current_date.month, current_date.day) < (value.month, value.day)))
 
 	def encode(self):
 
-		self.encoder_gender = sklearn.preprocessing.LabelEncoder()
-		self.encoder_location = sklearn.preprocessing.LabelEncoder()
-		self.encoder_category = sklearn.preprocessing.LabelEncoder()
+		match self.version:
 
-		self.data_subject['gender'] = self.encoder_gender.fit_transform(self.data_subject['gender'])
-		self.data_subject['location'] = self.encoder_location.fit_transform(self.data_subject['location'])
-		self.data_object['category'] = self.encoder_category.fit_transform(self.data_object['category'])
+			case 'base':
+
+				self.encoder_gender = sklearn.preprocessing.LabelEncoder()
+				self.encoder_location = sklearn.preprocessing.LabelEncoder()
+				self.encoder_category = sklearn.preprocessing.LabelEncoder()
+
+				self.data_subject['gender'] = self.encoder_gender.fit_transform(self.data_subject['gender'])
+				self.data_subject['location'] = self.encoder_location.fit_transform(self.data_subject['location'])
+				self.data_object['category'] = self.encoder_category.fit_transform(self.data_object['category'])
 
 	def merge(self):
 
-		self.data_subject = self.data_subject.add_prefix('subject_')
-		self.data_object = self.data_object.add_prefix('object_')
+		match self.version:
 
-		self.data = pandas.merge(self.data_action, self.data_subject, left_on = 'subject_id', right_on = 'subject_id', suffixes = ('_action', '_subject'))
-		self.data = pandas.merge(self.data, self.data_object, left_on = 'object_id', right_on = 'object_id', suffixes = ('_subject', '_object'))
+			case 'base':
+
+				self.data_subject = self.data_subject.add_prefix('subject_')
+				self.data_object = self.data_object.add_prefix('object_')
+
+				self.data = pandas.merge(self.data_action, self.data_subject, left_on = 'subject_id', right_on = 'subject_id', suffixes = ('_action', '_subject'))
+				self.data = pandas.merge(self.data, self.data_object, left_on = 'object_id', right_on = 'object_id', suffixes = ('_subject', '_object'))
 
 	def describe(self):
 
-		self.config = {}
+		match self.version:
 
-		self.config['dimension_subject'] = self.data['subject_id'].nunique()
-		self.config['dimension_object'] = self.data['object_id'].nunique()
-		self.config['dimension_gender'] = self.data['subject_gender'].nunique()
-		self.config['dimension_location'] = self.data['subject_location'].nunique()
-		self.config['dimension_category'] = self.data['object_category'].nunique()
+			case 'base':
 
-	def process(self):
+				self.config = {}
 
-		self.featuring()
-		self.encode()
-		self.merge()
-		self.describe()
+				self.config['dimension_subject'] = self.data['subject_id'].nunique()
+				self.config['dimension_object'] = self.data['object_id'].nunique()
+				self.config['dimension_gender'] = self.data['subject_gender'].nunique()
+				self.config['dimension_location'] = self.data['subject_location'].nunique()
+				self.config['dimension_category'] = self.data['object_category'].nunique()
 
-		self.dataset = self.Dataset(self.data)
-		self.dataloader = torch.utils.data.DataLoader(self.dataset, batch_size = self.batch_size, shuffle = True)
+	def process(self, data):
 
-		return self.config, self.dataloader
+		match self.version:
+
+			case 'base':
+
+				self.data_subject = data[0]
+				self.data_object = data[1]
+				self.data_action = data[2]
+
+				self.featuring()
+				self.encode()
+				self.merge()
+				self.describe()
+
+				self.dataset = self.Dataset(self.data, {'version': self.version})
+				self.dataloader = torch.utils.data.DataLoader(self.dataset, batch_size = self.batch_size, shuffle = True)
+
+		return self.dataloader
 
 		
--- src/util/script/build_script.py
+++ src/util/script/build_script.py
@@ -8,27 +8,28 @@ import sys
 import os
 import re
 
-def BuildScript(model_version, data_version = "base"):
+def BuildScript(model_version, data_version):
 
     data = src.data.load(data_version)
 
-    # add modes for data extraction
+    match data_version:
 
-    df_s = data['data'][0]
-    df_o = data['data'][1]
-    df_a = data['data'][2]
+        case 'base':
 
-    dvp = src.core.pipeline.DataValidationPipeline(df_s, df_o, df_a, { 'exclude': ['id', 'subject_id', 'object_id', 'birth', 'rate', 'timestamp'], 'time': ['birth', 'timestamp']})
-    df_clean_s, df_clean_o, df_clean_a = dvp.process()
+            dataframe_subject = data['data'][0]
+            dataframe_object = data['data'][1]
+            dataframe_action = data['data'][2]
 
-    mep = src.core.pipeline.ModelEmbeddingPipeline(df_clean_s,df_clean_o,df_clean_a, src.core.Config)
-    dc, dl = mep.process()
-    encoder_gender, encoder_category, encoder_location = mep.encoder_gender, mep.encoder_category, mep.encoder_location
-    engine = src.core.Engine()
-    model = engine.produce(model_version)
-    mtp = src.core.pipeline.ModelTrainingPipeline(model, dl)
-    mtp.train()
+            data_validation_pipeline = src.core.pipeline.DataValidationPipeline({'version': data_version})
+            data_clean = data_validation_pipeline.process([data['data'][0], data['data'][1], data['data'][2]], { 'exclude': ['id', 'subject_id', 'object_id', 'birth', 'rate', 'timestamp'], 'time': ['birth', 'timestamp'] })
+            
+            mep = src.core.pipeline.ModelEmbeddingPipeline({'version': data_version})
+            dl = mep.process(data_clean)
+            encoder_gender, encoder_category, encoder_location = mep.encoder_gender, mep.encoder_category, mep.encoder_location
+            engine = src.core.Engine()
+            model = engine.produce(model_version)
+            mtp = src.core.pipeline.ModelTrainingPipeline(model, dl, {'version': data_version})
+            mtp.train()
 
-
-    src.model.save(mtp.model, {'encoder_gender': encoder_gender, 'encoder_category': encoder_category, 'encoder_location': encoder_location}, 
-    {'version': model_version, 'dvp_config': { 'exclude': ['id', 'subject_id', 'object_id', 'subject_birth', 'rate', 'timestamp'], 'time': ['subject_birth', 'timestamp'] }})
+            src.model.save(mtp.model, {'encoder_gender': encoder_gender, 'encoder_category': encoder_category, 'encoder_location': encoder_location}, 
+            {'version': model_version, 'dvp_config': { 'exclude': ['id', 'subject_id', 'object_id', 'subject_birth', 'rate', 'timestamp'], 'time': ['subject_birth', 'timestamp'] }})
