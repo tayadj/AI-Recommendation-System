e236292263e89ab07eafa247e12e27a5d7129083
tayadj <tayadj@users.noreply.github.com>
2025-02-20 23:27:03 +0300

    Documentation enhancement

--- src/model/load.py
+++ src/model/load.py
@@ -6,6 +6,16 @@ import os
 
 def load(version):
 
+    """
+        Loads the model from the storage.
+
+        Args:
+            - version (str): The version of the model to load.
+
+        Returns:
+            - dict: The model, environment and config loaded from the specified version.
+    """
+
 	path = os.path.dirname(__file__) + '\\storage\\' + version
 
 	data = torch.load(path, weights_only = False)
