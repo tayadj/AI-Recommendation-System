959aae8b56f21d55dca4aa286c4687e7e235d4bc
tayadj <tayadj@users.noreply.github.com>
2025-02-20 23:19:38 +0300

    Added data documentation

--- src/data/save.py
+++ src/data/save.py
@@ -6,6 +6,14 @@ import os
 
 def save(dataframe, config):
 
+    """
+		Saves the data to the storage.
+
+		Args:
+		dataframe (pandas.DataFrame): The data to be saved.
+		config (dict): Configuration dictionary containing additional information.
+    """
+
 	path = os.path.dirname(__file__) + '\\storage\\' + config.get('version')
 
 	data = {
