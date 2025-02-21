f70872acb463bcdbcc0061ba6b1c58444db983eb
tayadj <tayadj@users.noreply.github.com>
2025-02-21 16:03:25 +0300

    Typo fixes

--- src/model/save.py
+++ src/model/save.py
@@ -6,14 +6,14 @@ import os
 
 def save(model, environment, config):
 
-    """
+	"""
 		Saves the Model to the storage.
 
 		Args:
 			- model (torch.nn.Module): The model to be saved.
 			- environment (dict): The model environment for inference.
 			- config (dict): Configuration dictionary containing additional information.
-    """
+	"""
 
 	path = os.path.dirname(__file__) + '\\storage\\' + config.get('version')
 
@@ -22,5 +22,5 @@ def save(model, environment, config):
 		'environment': environment,
 		'config': config
 	}
-
+	
 	torch.save(data, path)
